d249f25b884d9aacdd9b6930e44b39ebca58c756
vishaljangid786 <vishaljangid80550786@gmail.com>
2025-07-03 07:55:00 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -49,5 +49,10 @@ app.use("/api/messages", messageRouter);
 // connect to mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on port: ", PORT));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on port: ", PORT));
+}
+
+// export server for vercel
+export default server;
